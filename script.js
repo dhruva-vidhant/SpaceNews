e6cbe60b356fccb58bc8f4bf58e82ad3d4240493
dhruva-vidhant <66923429+dhruva-vidhant@users.noreply.github.com>
2022-12-10 23:05:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-
+function addEmail() {
+    const fs = require('fs')
+    //read json file and stores into variable json
+    let json = get("https://test.dhruvasrinivasa.repl.co/read", {
+        method: "GET",
+        headers: {},
+    })
+    //gets new email, name and location
+    var newEmail = Document.getElementById("email")
+    var newName = Document.getElementById("name")
+    navigator.geolocation.getCurrentPosition((position => {
+        let newLat = position.lat
+        let newLong = position.long
+    }, console.log));
+    //adds to json
+    json.add({
+        email: newEmail,
+        name: newName,
+        lat: newLat,
+        long: newLong,
+        hadIssRecently: 0
+    })
+    //updates file
+    get("https://Test.dhruvasrinivasa.repl.co/"+json, {
+        method: "GET",
+        headers: {}
+    })
+}
